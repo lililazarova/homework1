35d8ff5a9394795d2dd8c2425abfd5c2427af66e
lililazarova <lazarovalili@abv.bg>
2016-01-31 20:19:18 +0200

    finished

---
+++ zad2.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$d=15;
+echo "Кръг с d = $d има P =  ";
+echo 3.14*$d . 'сm .'. '<br/>';
+$r=20;
+echo "Кръг с r = $r има S =  ";
+echo (3.14*$r)*$r . 'сm .'. '<br/>';
+?>
